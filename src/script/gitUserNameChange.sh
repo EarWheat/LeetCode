c83d39ec9c22b48bf5e1916dacc0c5ccc5c8d5b9
EarWheat <2636965138@qq.com>
2019-09-19 17:42:53 +0800

    脚本commit

--- src/script/gitUserNameChange.sh
+++ src/script/gitUserNameChange.sh
@@ -4,13 +4,13 @@ set personalUserName EarWheat
 set personalUserEmail 2636965138@qq.com
 lindex $argv 0
 
-spawn git config --global user.name $personalUserName  $argv
+spawn git config --global user.name $personalUserName
 interact
 spawn git config --global user.email $personalUserEmail
 interact
 spawn git add .
 interact
-spawn git commit -m "commit_message" .
+spawn git commit -m $argv
 interact
 spawn git push origin master.
 interact
