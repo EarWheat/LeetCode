f2d8cc1ca0df18ef3f987b83133666b63ab5a6ff
EarWheat <2636965138@qq.com>
2019-09-19 17:41:34 +0800

    commit_message

--- src/script/gitUserNameChange.sh
+++ src/script/gitUserNameChange.sh
@@ -2,8 +2,9 @@
 set timeout 30
 set personalUserName EarWheat
 set personalUserEmail 2636965138@qq.com
+lindex $argv 0
 
-spawn git config --global user.name $personalUserName
+spawn git config --global user.name $personalUserName  $argv
 interact
 spawn git config --global user.email $personalUserEmail
 interact
