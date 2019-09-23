d12b26094361b757f2808bd2fb87157aeb00b5a1
EarWheat <2636965138@qq.com>
2019-09-23 19:56:42 +0800

    跳台阶

---
+++ src/leetcode/FrogJump/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.FrogJump;
+
+/*
+ * @author:liuzhaolu
+ * @createTime: 2019-09-23 19:39
+ * @desc:一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+ */
+public class Solution {
+    public int JumpFloor(int target) {
+        if(target == 1){
+            return 1;
+        }
+        if(target == 2){
+            return 2;
+        }
+        return JumpFloor(target - 1) + JumpFloor(target - 2);
+    }
+}
