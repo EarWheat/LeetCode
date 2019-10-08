46058c22a98e2d1535ccbcc19f97c510cfd0efad
EarWheat <2636965138@qq.com>
2019-10-08 20:06:51 +0800

    矩形覆盖

---
+++ src/leetcode/RectCover/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.RectCover;
+
+/*
+ * @author:liuzhaolu
+ * @createTime: 2019-10-08 19:35
+ * @desc:我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+ */
+public class Solution {
+    public int RectCover(int target) {
+        if (target < 1) {
+            return 0;
+        } else if (target == 1 || target == 2) {
+            return target;
+        } else {
+            return RectCover(target-1) + RectCover(target-2);
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(new Solution().RectCover(4));
+    }
+}
