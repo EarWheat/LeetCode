b234b0e0e17f91656f8fa469111b30fb91dd20fd
EarWheat <2636965138@qq.com>
2020-01-26 12:44:21 +0800

    镜像树

---
+++ src/leetcode/MirrorTree/MirrorTree.java
@@ -0,0 +1,37 @@
+package leetcode.MirrorTree;
+
+import leetcode.tree.TreeNode;
+
+/*
+ * @author:liuzhaolu
+ * @createTime: 2020-01-26 10:54
+ * @desc:
+ */
+public class MirrorTree {
+    public static void main(String[] args){
+
+    }
+
+    public void Mirror(TreeNode root) {
+        if(root == null)
+        {
+            return;
+        }
+        if(root.getLeft() == null && root.getRight() == null)
+        {
+            return;
+        }
+        TreeNode temp;
+        temp = root.getRight();
+        root.getRight() = root.getLeft();
+        root.getLeft() = temp;
+        if(root.left != null)
+        {
+            Mirror(root.left);
+        }
+        if(root.right != null)
+        {
+            Mirror(root.right);
+        }
+    }
+}
