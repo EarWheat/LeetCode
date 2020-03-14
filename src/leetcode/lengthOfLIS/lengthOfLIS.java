6d6405abb0a38982fe1a5e81065dd99dae428dcb
EarWheat <2636965138@qq.com>
2020-03-14 19:53:38 +0800

    最长子序列

---
+++ src/leetcode/lengthOfLIS/lengthOfLIS.java
@@ -0,0 +1,46 @@
+package leetcode.lengthOfLIS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * @author:liuzhaolu
+ * @createTime: 2020-03-14 10:11
+ * @desc:给定一个无序的整数数组，找到其中最长上升子序列的长度。
+ * 输入: [10,9,2,5,3,7,101,18]
+输出: 4
+解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+ */
+public class lengthOfLIS {
+    public static void main(String[] args){
+        int[] array = new int[]{10,9,2,5,3,7,101,18};
+        System.out.println(lengthOfLIS(array));
+    }
+
+    private static int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return len;
+        }
+        int[] dp = new int[len];
+        // 自己一定是一个子序列
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < len; i++) {
+            // 看以前的，比它小的，说明可以接在后面形成一个更长的子序列
+            // int curMax = Integer.MIN_VALUE; 不能这样写，万一前面没有比自己小的，
+            // 这个值就得不到更新
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+
+        int res = dp[0];
+        for (int i = 0; i < len; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
