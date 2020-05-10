351bbd47202e78f3adbf092b2ac507b66d217e60
EarWheat <2636965138@qq.com>
2020-05-10 15:49:40 +0800

    最近公共祖先

---
+++ src/main/java/leetcode/lowestCommonAncestor/TreeNode.java
@@ -0,0 +1,13 @@
+package main.java.leetcode.lowestCommonAncestor;
+
+/*
+ * @author:liuzhaolu
+ * @createTime: 2020-05-07 10:44
+ * @desc:
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
