ac0e19474552a9bd0c7c92573c0071de6793db33
EarWheat <2636965138@qq.com>
2020-05-26 10:07:23 +0800

    快慢指针

---
+++ src/main/java/leetcode/findDuplicate/answer.java
@@ -0,0 +1,22 @@
+package leetcode.findDuplicate;
+
+/*
+ * @author:liuzhaolu
+ * @createTime: 2020-05-26 10:05
+ * @desc: 快慢指针算法
+ */
+public class answer {
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
