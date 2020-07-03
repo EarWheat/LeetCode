2ec06c7f42b53de52240fbdbc2933943e44d212a
EarWheat <2636965138@qq.com>
2020-07-03 12:53:32 +0800

    sortedArrayToBST

---
+++ src/main/java/leetcode/sortedArrayToBST/TreeNode.java
@@ -0,0 +1,13 @@
+package leetcode.sortedArrayToBST;
+
+/*
+ * @author:liuzhaolu
+ * @createTime: 2020-07-03 10:47
+ * @desc:
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
