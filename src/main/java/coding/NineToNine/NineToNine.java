fcdce4449b6edeb8b8fe557175fe4200a6b26538
EarWheat <2636965138@qq.com>
2020-10-13 16:41:58 +0800

    99乘法表右对齐

--- src/main/java/coding/NineToNine/NineToNine.java
+++ src/main/java/coding/NineToNine/NineToNine.java
@@ -12,8 +12,15 @@ package coding.NineToNine;
 public class NineToNine {
     public static void main(String[] args) {
         for(int i = 1;i <= 9; i++){
-            for(int j = 1; j <= i; j++){
-                System.out.print(j + " * " + i + " = " + i * j + "  ");
+            for(int k = 1; k < i; k++){
+                System.out.print("       ");
+            }
+            for(int j = i; j <= 9; j++){
+                if(i * j < 10){
+                    System.out.print(i + "*" + j + "=" + i * j + "  ");
+                } else {
+                    System.out.print(i + "*" + j + "=" + i * j + " ");
+                }
             }
             System.out.println();
         }
