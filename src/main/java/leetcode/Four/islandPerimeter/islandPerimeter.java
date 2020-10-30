b4603ea3ceec71ce3fba5026022b7a2aa4bcba8f
EarWheat <2636965138@qq.com>
2020-10-30 17:40:37 +0800

    islandPerimeter岛的周长

---
+++ src/main/java/leetcode/Four/islandPerimeter/islandPerimeter.java
@@ -0,0 +1,80 @@
+package leetcode.Four.islandPerimeter;
+
+/**
+ * @author liuzhaoluliuzhaolu
+ * @date 2020-10-30 17:05
+ * @desc 给定一个包含 0 和 1 的二维网格地图，其中 1 表示陆地 0 表示水域。
+ *
+ * 网格中的格子水平和垂直方向相连（对角线方向不相连）。整个网格被水完全包围，但其中恰好有一个岛屿（或者说，一个或多个表示陆地的格子相连组成的岛屿）。
+ *
+ * 岛屿中没有“湖”（“湖” 指水域在岛屿内部且不和岛屿周围的水相连）。格子是边长为 1 的正方形。网格为长方形，且宽度和高度均不超过 100 。计算这个岛屿的周长。
+ *
+ *  
+ *
+ * 示例 :
+ *
+ * 输入:
+ * [[0,1,0,0],
+ *  [1,1,1,0],
+ *  [0,1,0,0],
+ *  [1,1,0,0]]
+ *
+ * 输出: 16
+ *
+ * 解释: 它的周长是下面图片中的 16 个黄色的边：
+ *
+ *
+ *
+ *
+ * 链接：https://leetcode-cn.com/problems/island-perimeter
+ * @prd
+ * @Modification History:
+ * Date         Author          Description
+ * ------------------------------------------ *
+ */
+public class islandPerimeter {
+    public int islandPerimeter(int[][] grid) {
+        int result = 0;
+        if(grid.length <= 0){
+            return 0;
+        }
+        for(int i = 0;i < grid.length;i++){
+            for(int j = 0;j < grid[0].length;j++){
+                if(grid[i][j] == 1){
+                    int numOfNeighbor = getNeighbor(i,j,grid);
+                    result += 4 - numOfNeighbor;
+                }
+            }
+        }
+        return result;
+    }
+
+    public int getNeighbor(int i, int j, int[][] grid){
+        int neighbor = 0;
+        // 上
+        if(i > 0){
+            if(grid[i-1][j] == 1){
+                neighbor++;
+            }
+        }
+        // 下
+        if(i < grid.length - 1){
+            if(grid[i+1][j] == 1){
+                neighbor++;
+            }
+        }
+        // 左
+        if(j > 0){
+            if(grid[i][j - 1] == 1){
+                neighbor++;
+            }
+        }
+        // 右
+        if(j < grid[0].length - 1){
+            if(grid[i][j + 1] == 1){
+                neighbor++;
+            }
+        }
+        return neighbor;
+    }
+}
