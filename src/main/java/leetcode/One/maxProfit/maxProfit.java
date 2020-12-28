8971953b540e7d4c002b41fbf96d076c7b319fd0
EarWheat <2636965138@qq.com>
2020-12-28 14:46:59 +0800

    maxProfit买卖股票的最大利润

--- src/main/java/leetcode/One/maxProfit/maxProfit.java
+++ src/main/java/leetcode/One/maxProfit/maxProfit.java
@@ -60,10 +60,10 @@ public class maxProfit {
     public static int maxProfit(int[] prices) {
         int[][] dp = new int[prices.length][3];
         dp[0][0] = 0;
-        dp[0][1] = -7;
+        dp[0][1] = -prices[0];
         for(int i = 1; i < prices.length;i++){
             dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1],dp[i-1][0] - prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
         }
         return dp[prices.length - 1][0];
     }
