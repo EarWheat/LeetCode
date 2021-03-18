ae2615f0e82d5da5ec775debdd3c47aa06d51147
EarWheat <2636965138@qq.com>
2021-03-19 07:44:45 +0800

    ParkingSystem

--- src/main/java/coding/Thread/ThreadTest.java
+++ src/main/java/coding/Thread/ThreadTest.java
@@ -1,5 +1,7 @@
 package coding.Thread;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -7,6 +9,10 @@ import java.util.concurrent.CountDownLatch;
  * @version create_time：2018/9/12 类说明:
  */
 public class ThreadTest {
+    public static StringBuffer stringBuffer = new StringBuffer();
+    public static StringBuilder stringBuilder = new StringBuilder();
+    public static int num = 0;
+
     public static void main(String[] args) throws InterruptedException {
 //
 //        MyThread thread = new MyThread();
@@ -15,14 +21,16 @@ public class ThreadTest {
 //        MyRunnable myRunnable = new MyRunnable();
 //        Thread runnable = new Thread(myRunnable);
 //        runnable.start();	//开启一个线程
+        List list = new ArrayList();
         CountDownLatch countDownLatch = new CountDownLatch(2);
-        StringBuffer stringBuffer = new StringBuffer();
-        StringRunnable runnable = new StringRunnable(stringBuffer, countDownLatch);
+        StaticRunnable runnable = new StaticRunnable(countDownLatch);
         Thread thread = new Thread(runnable);
         Thread thread1 = new Thread(runnable);
         thread.start();
         thread1.start();
         countDownLatch.await();
         System.out.println(stringBuffer.length());
+        System.out.println(stringBuilder.length());
+        System.out.println("=========="+num);
     }
 }
