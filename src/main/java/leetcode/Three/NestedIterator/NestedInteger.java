43e83e2ff0469061d5c8f028fa8d8062efa24807
EarWheat <2636965138@qq.com>
2021-03-23 07:30:58 +0800

    扁平化迭代器

---
+++ src/main/java/leetcode/Three/NestedIterator/NestedInteger.java
@@ -0,0 +1,18 @@
+package leetcode.Three.NestedIterator;
+
+import java.util.List;
+
+/**
+ * @author liuzhaoluliuzhaolu
+ * @date 2021/3/23 上午7:20
+ * @desc
+ * @prd
+ * @Modification History:
+ * Date         Author          Description
+ * ------------------------------------------ *
+ */
+public interface NestedInteger {
+    public boolean isInteger();
+    public Integer getInteger();
+    public List<NestedInteger> getList();
+}
