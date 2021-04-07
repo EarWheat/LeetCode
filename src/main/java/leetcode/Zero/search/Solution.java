5bcd00ffa3a6dc36683ef2dcfba6ecd1d93604c2
EarWheat <2636965138@qq.com>
2021-04-07 16:10:37 +0800

    cv的一天

---
+++ src/main/java/leetcode/Zero/search/Solution.java
@@ -0,0 +1,48 @@
+package leetcode.Zero.search;
+
+/**
+ * @author liuzhaoluliuzhaolu
+ * @date 2021/4/7 下午2:12
+ * @desc
+ * @prd
+ * @Modification History:
+ * Date         Author          Description
+ * ------------------------------------------ *
+ */
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return nums[0] == target;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[l] == nums[mid] && nums[mid] == nums[r]) {
+                ++l;
+                --r;
+            } else if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return false;
+
+    }
+
+}
