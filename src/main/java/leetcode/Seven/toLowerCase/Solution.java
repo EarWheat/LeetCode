7a7323611735f8145678b4aa375ac23169a0c42e
EarWheat <2636965138@qq.com>
2021-12-12 16:13:58 +0800

    转换成小写字母

---
+++ src/main/java/leetcode/Seven/toLowerCase/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.Seven.toLowerCase;
+
+/**
+ * @author ：liuzhaolu
+ * @description：TODO
+ * @prd :
+ * @date ：2021/12/12 4:13 下午
+ * @Modification Date         Author          Description
+ * ------------------------------------------ *
+ * 2021/12/12 4:13 下午     liuzhaolu       firstVersion
+ */
+public class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
