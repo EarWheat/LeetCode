6b3280b6c14372cf6a08a2a166e2856f925ade31
EarWheat <2636965138@qq.com>
2022-01-09 17:30:42 +0800

    atomicInteger

--- src/main/java/leetcode/One/Thousand/Foo/Foo.java
+++ src/main/java/leetcode/One/Thousand/Foo/Foo.java
@@ -19,7 +19,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Foo {
 
 
-    private volatile int flag = 1;
+    private AtomicInteger flag = new AtomicInteger(1);
 
     public Foo() {
 
@@ -28,18 +28,18 @@ public class Foo {
     public void first(Runnable printFirst) throws InterruptedException {
         // printFirst.run() outputs "first". Do not change or remove this line.
         printFirst.run();
-        flag = 2;
+        flag.getAndIncrement();
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        while(flag != 2);
+        while(flag.get() != 2);
         // printSecond.run() outputs "second". Do not change or remove this line.
         printSecond.run();
-        flag = 3;
+        flag.getAndIncrement();
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        while(flag != 3);
+        while(flag.get() != 3);
         // printThird.run() outputs "third". Do not change or remove this line.
         printThird.run();
     }
