c4a4f5265f72404661a1bb3cb1fcf83caeea5d43
EarWheat <2636965138@qq.com>
2022-01-12 14:15:32 +0800

    答案

--- src/main/java/leetcode/Three/increasingTriplet/Solution.java
+++ src/main/java/leetcode/Three/increasingTriplet/Solution.java
@@ -48,6 +48,25 @@ public class Solution {
         return false;
     }
 
+    public boolean answer(int[] nums){
+        int n = nums.length;
+        if (n < 3) {
+            return false;
+        }
+        int first = nums[0], second = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            if (num > second) {
+                return true;
+            } else if (num > first) {
+                second = num;
+            } else {
+                first = num;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.increasingTriplet(new int[]{4,5,2147483647,1,2}));
