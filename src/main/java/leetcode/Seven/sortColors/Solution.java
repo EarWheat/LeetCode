f40a3f506c2dd7bb2fd4357a8cb6d0ec4ea5319d
EarWheat <2636965138@qq.com>
2022-01-17 15:44:00 +0800

    膜拜

--- src/main/java/leetcode/Seven/sortColors/Solution.java
+++ src/main/java/leetcode/Seven/sortColors/Solution.java
@@ -36,7 +36,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{2,0,1};
+        int[] nums = new int[]{2,0,2,1,1,0};
         Solution solution = new Solution();
         solution.sortColors(nums);
     }
