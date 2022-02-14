82dcccbb5f875a1ea1a1e09b79c249bb5ff9b7fb
EarWheat <2636965138@qq.com>
2022-02-14 18:21:16 +0800

    快拍

--- src/main/java/coding/Sort/BubbleSort.java
+++ src/main/java/coding/Sort/BubbleSort.java
@@ -1,5 +1,7 @@
 package coding.Sort;
 
+import java.util.Arrays;
+
 /**
  * @author liuzhaolu
  * @version create_time：2018/8/1 类说明:冒泡排序
@@ -8,13 +10,13 @@ public class BubbleSort {
 
 
     //从小到大排序
-    public Integer[] BubbleSort(Integer[] array){
-        for (int i = 1;i<array.length;i++){
-            for (int j = 0;j < array.length-i;j++){
-                if(array[j+1] < array[j]){
+    public Integer[] BubbleSort(Integer[] array) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 0; j < array.length - i; j++) {
+                if (array[j + 1] < array[j]) {
                     int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
 
             }
@@ -22,18 +24,38 @@ public class BubbleSort {
         return array;
     }
 
-    public void showArray(Integer[] array){
-        for(int i = 0;i<array.length;i++){
+    public void BubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length - i; j++) {
+                if (nums[j] < nums[j - 1]) {
+                    swap(nums, j - 1, j);
+                }
+            }
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void showArray(Integer[] array) {
+        for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]);
             System.out.print(" ");
         }
     }
 
-    public static void main(String[] args){
-        Integer array[] = {23,45,11,18,45,66,78};
+    public static void main(String[] args) {
+//        Integer array[] = {23, 45, 11, 18, 45, 66, 78};
+//        BubbleSort bubbleSort = new BubbleSort();
+//        bubbleSort.showArray(array);
+//        System.out.println();
+//        bubbleSort.showArray(bubbleSort.BubbleSort(array));
+        int[] nums = new int[]{23, 45, 11, 18, 45, 66, 78};
         BubbleSort bubbleSort = new BubbleSort();
-        bubbleSort.showArray(array);
-        System.out.println();
-        bubbleSort.showArray(bubbleSort.BubbleSort(array));
+        bubbleSort.BubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
--- src/main/java/coding/Sort/QuickSort.java
+++ src/main/java/coding/Sort/QuickSort.java
@@ -1,39 +1,66 @@
 package coding.Sort;
 
+import java.util.Arrays;
+
 /*
  * @author:liuzhaolu
  * @createTime: 2020-03-23 14:04
  * @desc:
  */
 public class QuickSort {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        int[] nums = new int[]{23, 45, 11, 18, 45, 66, 78};
+        QuickSort quickSort = new QuickSort();
+        quickSort.quickSort(nums, 0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
     }
 
-    public static void sort(int[] a, int low, int high){
-        if(low >= high){
+    public void quickSort(int[] nums, int left, int right) {
+        if(left >= right){
             return;
         }
-
-        int left = low;
-        int right = high;
-
-        // 保存基准值
-        int index = a[left];
-        while (left < right){
-            // 从后向前找比基准小的元素
-            while (left < right && a[right] >= index){
-                right--;
-            }
-            a[left] = a[right];
-            // 从前往后找比基准大的元素
-            while (left < right && a[left] <= index){
-                left++;
-            }
-            a[right] = a[left];
+        int index = left;
+        int temp = nums[index];
+        while (left < right) {
+            while (left < right && nums[right] >= temp) right--;
+            while (left < right && nums[left] <= temp) left++;
+            swap(nums, left, right);
         }
-        a[left] = index;
-        sort(a, low, left - 1);
-        sort(a, left + 1, high);
+        swap(nums, index, left);
+        quickSort(nums, 0, left - 1);
+        quickSort(nums, left + 1, right);
     }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+//    public static void sort(int[] a, int low, int high){
+//        if(low >= high){
+//            return;
+//        }
+//
+//        int left = low;
+//        int right = high;
+//
+//        // 保存基准值
+//        int index = a[left];
+//        while (left < right){
+//            // 从后向前找比基准小的元素
+//            while (left < right && a[right] >= index){
+//                right--;
+//            }
+//            a[left] = a[right];
+//            // 从前往后找比基准大的元素
+//            while (left < right && a[left] <= index){
+//                left++;
+//            }
+//            a[right] = a[left];
+//        }
+//        a[left] = index;
+//        sort(a, low, left - 1);
+//        sort(a, left + 1, high);
+//    }
+
 }
