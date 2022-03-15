de2027aac7eda6cec010f1dda07d774549eb09e7
EarWheat <2636965138@qq.com>
2022-03-15 17:06:23 +0800

    重排链表

---
+++ src/main/java/leetcode/One/reorderList/Solution.java
@@ -0,0 +1,63 @@
+package leetcode.One.reorderList;
+
+import leetcode.Util.ListNode;
+
+/**
+ * @author ：liuzhaolu
+ * @description：143. 重排链表
+ * @prd : https://leetcode-cn.com/problems/reorder-list/solution/zhong-pai-lian-biao-by-leetcode-solution/
+ * @date ：2022/3/15 5:05 下午
+ * @Modification Date         Author          Description
+ * ------------------------------------------ *
+ * 2022/3/15 5:05 下午     liuzhaolu       firstVersion
+ */
+public class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        ListNode mid = middleNode(head);
+        ListNode l1 = head;
+        ListNode l2 = mid.next;
+        mid.next = null;
+        l2 = reverseList(l2);
+        mergeList(l1, l2);
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    public void mergeList(ListNode l1, ListNode l2) {
+        ListNode l1_tmp;
+        ListNode l2_tmp;
+        while (l1 != null && l2 != null) {
+            l1_tmp = l1.next;
+            l2_tmp = l2.next;
+
+            l1.next = l2;
+            l1 = l1_tmp;
+
+            l2.next = l1;
+            l2 = l2_tmp;
+        }
+    }
+}
