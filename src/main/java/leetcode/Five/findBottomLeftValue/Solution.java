45dc6c2c7483dfb1a421fc4bbfb6e3c8d7601d68
EarWheat <2636965138@qq.com>
2022-06-22 16:42:03 +0800

    找树左下角的值

---
+++ src/main/java/leetcode/Five/findBottomLeftValue/Solution.java
@@ -0,0 +1,35 @@
+package leetcode.Five.findBottomLeftValue;
+
+import leetcode.Util.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * @Desc:
+ * @Author: 泽露
+ * @Date: 2022/6/22 4:34 PM
+ * @Version: 1.initial version; 2022/6/22 4:34 PM
+ */
+public class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        TreeNode result = null;
+        while (!queue.isEmpty()){
+            result = queue.peek();
+            Queue<TreeNode> temp = new ArrayDeque<>();
+            while (!queue.isEmpty()){
+                TreeNode node = queue.poll();
+                if(node.left != null){
+                    temp.add(node.left);
+                }
+                if(node.right != null){
+                    temp.add(node.right);
+                }
+            }
+            queue = temp;
+        }
+        return result != null ? result.val : 0;
+    }
+}
