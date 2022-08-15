272a34fb5c404cf2fd6b0609eea3f585169fd484
EarWheat <2636965138@qq.com>
2022-08-15 17:29:38 +0800

    头尾指针

--- src/main/java/leetcode/Six/MyCircularDeque/MyCircularDeque.java
+++ src/main/java/leetcode/Six/MyCircularDeque/MyCircularDeque.java
@@ -15,35 +15,47 @@ public class MyCircularDeque {
         tail = k - 1;
     }
     public boolean insertFront(int value) {
-        if (isFull()) return false;
+        if (isFull()) {
+            return false;
+        }
         elements[head = head == 0 ? elements.length - 1 : head - 1] = value;// 头指针左移
         size++;
         return true;
     }
     public boolean insertLast(int value) {
-        if (isFull()) return false;
+        if (isFull()) {
+            return false;
+        }
         elements[tail = tail == elements.length - 1 ? 0 : tail + 1] = value;// 尾指针右移
         size++;
         return true;
     }
     public boolean deleteFront() {
-        if (isEmpty()) return false;
+        if (isEmpty()) {
+            return false;
+        }
         head = head == elements.length - 1 ? 0 : head + 1;// 头指针右移
         size--;
         return true;
     }
     public boolean deleteLast() {
-        if (isEmpty()) return false;
+        if (isEmpty()) {
+            return false;
+        }
         tail = tail == 0 ? elements.length - 1 : tail - 1;// 尾指针左移
         size--;
         return true;
     }
     public int getFront() {
-        if (isEmpty()) return -1;
+        if (isEmpty()) {
+            return -1;
+        }
         return elements[head];
     }
     public int getRear() {
-        if (isEmpty()) return -1;
+        if (isEmpty()) {
+            return -1;
+        }
         return elements[tail];
     }
     public boolean isEmpty() {
