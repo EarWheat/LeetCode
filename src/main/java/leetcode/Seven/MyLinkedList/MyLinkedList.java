2842a070d5dbd2ea083fe127508ef498ba928d77
EarWheat <2636965138@qq.com>
2022-09-23 18:26:55 +0800

    双向链表

---
+++ src/main/java/leetcode/Seven/MyLinkedList/MyLinkedList.java
@@ -0,0 +1,145 @@
+package leetcode.Seven.MyLinkedList;
+//设计链表的实现。您可以选择使用单链表或双链表。单链表中的节点应该具有两个属性：val 和 next。val 是当前节点的值，next 是指向下一个节点的指针
+///引用。如果要使用双向链表，则还需要一个属性 prev 以指示链表中的上一个节点。假设链表中的所有节点都是 0-index 的。
+//
+// 在链表类中实现这些功能：
+//
+//
+// get(index)：获取链表中第 index 个节点的值。如果索引无效，则返回-1。
+// addAtHead(val)：在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。
+// addAtTail(val)：将值为 val 的节点追加到链表的最后一个元素。
+// addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val 的节点。如果 index 等于链表的长度，则该节点将附加
+//到链表的末尾。如果 index 大于链表长度，则不会插入节点。如果index小于0，则在头部插入节点。
+// deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。
+//
+//
+//
+//
+// 示例：
+//
+// MyLinkedList linkedList = new MyLinkedList();
+//linkedList.addAtHead(1);
+//linkedList.addAtTail(3);
+//linkedList.addAtIndex(1,2);   //链表变为1-> 2-> 3
+//linkedList.get(1);            //返回2
+//linkedList.deleteAtIndex(1);  //现在链表是1-> 3
+//linkedList.get(1);            //返回3
+//
+//
+//
+//
+// 提示：
+//
+//
+// 所有val值都在 [1, 1000] 之内。
+// 操作次数将在 [1, 1000] 之内。
+// 请不要使用内置的 LinkedList 库。
+//
+// Related Topics 设计 链表 👍 612 👎 0
+
+/**
+ * @Desc:
+ * @Author: 泽露
+ * @Date: 2022/9/23 4:14 PM
+ * @Version: 1.initial version; 2022/9/23 4:14 PM
+ */
+class MyLinkedList {
+    int size;
+    ListNode head;
+    ListNode tail;
+
+    public MyLinkedList() {
+        size = 0;
+        head = new ListNode(0);
+        tail = new ListNode(0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+        ListNode curr;
+        if (index + 1 < size - index) {
+            curr = head;
+            for (int i = 0; i <= index; i++) {
+                curr = curr.next;
+            }
+        } else {
+            curr = tail;
+            for (int i = 0; i < size - index; i++) {
+                curr = curr.prev;
+            }
+        }
+        return curr.val;
+    }
+
+    public void addAtHead(int val) {
+        addAtIndex(0, val);
+    }
+
+    public void addAtTail(int val) {
+        addAtIndex(size, val);
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index > size) {
+            return;
+        }
+        index = Math.max(0, index);
+        ListNode pred, succ;
+        if (index < size - index) {
+            pred = head;
+            for (int i = 0; i < index; i++) {
+                pred = pred.next;
+            }
+            succ = pred.next;
+        } else {
+            succ = tail;
+            for (int i = 0; i < size - index; i++) {
+                succ = succ.prev;
+            }
+            pred = succ.prev;
+        }
+        size++;
+        ListNode toAdd = new ListNode(val);
+        toAdd.prev = pred;
+        toAdd.next = succ;
+        pred.next = toAdd;
+        succ.prev = toAdd;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
+        ListNode pred, succ;
+        if (index < size - index) {
+            pred = head;
+            for (int i = 0; i < index; i++) {
+                pred = pred.next;
+            }
+            succ = pred.next.next;
+        } else {
+            succ = tail;
+            for (int i = 0; i < size - index - 1; i++) {
+                succ = succ.prev;
+            }
+            pred = succ.prev.prev;
+        }
+        size--;
+        pred.next = succ;
+        succ.prev = pred;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode prev;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
