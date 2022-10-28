e1f4b98b94499c6a22fb5c0d289584c20ea4a7bf
EarWheat <2636965138@qq.com>
2022-10-28 11:26:32 +0800

    {一周CV 3次}

---
+++ src/main/java/leetcode/Nine/sumSubarrayMins/Solution.java
@@ -0,0 +1,73 @@
+package leetcode.Nine.sumSubarrayMins;
+//给定一个整数数组 arr，找到 min(b) 的总和，其中 b 的范围为 arr 的每个（连续）子数组。
+//
+// 由于答案可能很大，因此 返回答案模 10^9 + 7 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：arr = [3,1,2,4]
+//输出：17
+//解释：
+//子数组为 [3]，[1]，[2]，[4]，[3,1]，[1,2]，[2,4]，[3,1,2]，[1,2,4]，[3,1,2,4]。
+//最小值为 3，1，2，4，1，1，2，1，1，1，和为 17。
+//
+// 示例 2：
+//
+//
+//输入：arr = [11,81,94,43,3]
+//输出：444
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= arr.length <= 3 * 10⁴
+// 1 <= arr[i] <= 3 * 10⁴
+//
+//
+//
+// Related Topics 栈 数组 动态规划 单调栈 👍 475 👎 0
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @Desc:
+ * @Author: 泽露
+ * @Date: 2022/10/28 11:19 AM
+ * @Version: 1.initial version; 2022/10/28 11:19 AM
+ */
+public class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        Deque<Integer> monoStack = new ArrayDeque<Integer>();
+        int[] left = new int[n];
+        int[] right = new int[n];
+        for (int i = 0; i < n; i++) {
+            while (!monoStack.isEmpty() && arr[i] <= arr[monoStack.peek()]) {
+                monoStack.pop();
+            }
+            left[i] = i - (monoStack.isEmpty() ? -1 : monoStack.peek());
+            monoStack.push(i);
+        }
+        monoStack.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!monoStack.isEmpty() && arr[i] < arr[monoStack.peek()]) {
+                monoStack.pop();
+            }
+            right[i] = (monoStack.isEmpty() ? n : monoStack.peek()) - i;
+            monoStack.push(i);
+        }
+        long ans = 0;
+        final int MOD = 1000000007;
+        for (int i = 0; i < n; i++) {
+            ans = (ans + (long) left[i] * right[i] * arr[i]) % MOD;
+        }
+        return (int) ans;
+    }
+}
