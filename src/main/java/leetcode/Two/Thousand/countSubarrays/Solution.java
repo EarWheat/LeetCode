7a761306cd97b9f10a31515a56af529a2259e05d
EarWheat <2636965138@qq.com>
2023-03-16 19:21:15 +0800

    统计中位数为K的子数组

---
+++ src/main/java/leetcode/Two/Thousand/countSubarrays/Solution.java
@@ -0,0 +1,45 @@
+package leetcode.Two.Thousand.countSubarrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Desc:
+ * @Author: 泽露
+ * @Date: 2023/3/16 7:20 PM
+ * @Version: 1.initial version; 2023/3/16 7:20 PM
+ */
+public class Solution {
+    public int countSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        int kIndex = -1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == k) {
+                kIndex = i;
+                break;
+            }
+        }
+        int ans = 0;
+        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        counts.put(0, 1);
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += sign(nums[i] - k);
+            if (i < kIndex) {
+                counts.put(sum, counts.getOrDefault(sum, 0) + 1);
+            } else {
+                int prev0 = counts.getOrDefault(sum, 0);
+                int prev1 = counts.getOrDefault(sum - 1, 0);
+                ans += prev0 + prev1;
+            }
+        }
+        return ans;
+    }
+
+    public int sign(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        return num > 0 ? 1 : -1;
+    }
+}
