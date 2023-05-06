a111db1b81c1f3693883fe3a73e6943a4c399cff
EarWheat <2636965138@qq.com>
2023-05-06 14:13:57 +0800

    数青蛙

--- src/main/java/leetcode/One/Thousand/minNumberOfFrogs/Solution.java
+++ src/main/java/leetcode/One/Thousand/minNumberOfFrogs/Solution.java
@@ -44,52 +44,55 @@ package leetcode.One.Thousand.minNumberOfFrogs;
 // 字符串中的字符只有 'c', 'r', 'o', 'a' 或者 'k'
 //
 // Related Topics 字符串 计数 👍 146 👎 0
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * @Desc:
+ * @Desc: 数青蛙
  * @Author: 泽露
  * @Date: 2023/5/6 2:10 PM
  * @Version: 1.initial version; 2023/5/6 2:10 PM
  */
 public class Solution {
     public int minNumberOfFrogs(String croakOfFrogs) {
-        public int minNumberOfFrogs(String croakOfFrogs) {
-            if (croakOfFrogs.length() % 5 != 0) {
-                return -1;
-            }
-            int res = 0, frogNum = 0;
-            int[] cnt = new int[4];
-            Map<Character, Integer> map = new HashMap<Character, Integer>() {{
-                put('c', 0);
-                put('r', 1);
-                put('o', 2);
-                put('a', 3);
-                put('k', 4);
-            }};
-            for (int i = 0; i < croakOfFrogs.length(); i++) {
-                char c = croakOfFrogs.charAt(i);
-                int t = map.get(c);
-                if (t == 0) {
-                    cnt[t]++;
-                    frogNum++;
-                    if (frogNum > res) {
-                        res = frogNum;
-                    }
+        if (croakOfFrogs.length() % 5 != 0) {
+            return -1;
+        }
+        int res = 0, frogNum = 0;
+        int[] cnt = new int[4];
+        Map<Character, Integer> map = new HashMap<Character, Integer>() {{
+            put('c', 0);
+            put('r', 1);
+            put('o', 2);
+            put('a', 3);
+            put('k', 4);
+        }};
+        for (int i = 0; i < croakOfFrogs.length(); i++) {
+            char c = croakOfFrogs.charAt(i);
+            int t = map.get(c);
+            if (t == 0) {
+                cnt[t]++;
+                frogNum++;
+                if (frogNum > res) {
+                    res = frogNum;
+                }
+            } else {
+                if (cnt[t - 1] == 0) {
+                    return -1;
+                }
+                cnt[t - 1]--;
+                if (t == 4) {
+                    frogNum--;
                 } else {
-                    if (cnt[t - 1] == 0) {
-                        return -1;
-                    }
-                    cnt[t - 1]--;
-                    if (t == 4) {
-                        frogNum--;
-                    } else {
-                        cnt[t]++;
-                    }
+                    cnt[t]++;
                 }
             }
-            if (frogNum > 0) {
-                return -1;
-            }
-            return res;
         }
+        if (frogNum > 0) {
+            return -1;
+        }
+        return res;
     }
+}
 }
