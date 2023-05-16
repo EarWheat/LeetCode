5232da253730eb7dbafb668384fc705e179b5a58
EarWheat <2636965138@qq.com>
2023-05-16 16:06:27 +0800

    工作计划的最低难度

---
+++ src/main/java/leetcode/One/Thousand/minDifficulty/Solution.java
@@ -0,0 +1,37 @@
+package leetcode.One.Thousand.minDifficulty;
+
+import java.util.Arrays;
+
+/**
+ * @Desc: 工作计划的最低难度
+ * @Author: 泽露
+ * @Date: 2023/5/16 4:03 PM
+ * @Version: 1.initial version; 2023/5/16 4:03 PM
+ */
+public class Solution {
+    public int minDifficulty(int[] jobDifficulty, int d) {
+        int n = jobDifficulty.length;
+        if (n < d) {
+            return -1;
+        }
+        int[][] dp = new int[d + 1][n];
+        for (int i = 0; i <= d; ++i) {
+            Arrays.fill(dp[i], 0x3f3f3f3f);
+        }
+        int ma = 0;
+        for (int i = 0; i < n; ++i) {
+            ma = Math.max(ma, jobDifficulty[i]);
+            dp[0][i] = ma;
+        }
+        for (int i = 1; i < d; ++i) {
+            for (int j = i; j < n; ++j) {
+                ma = 0;
+                for (int k = j; k >= i; --k) {
+                    ma = Math.max(ma, jobDifficulty[k]);
+                    dp[i][j] = Math.min(dp[i][j], ma + dp[i - 1][k - 1]);
+                }
+            }
+        }
+        return dp[d - 1][n - 1];
+    }
+}
