318070aed3e77480bba83459db47c3f461017808
EarWheat <2636965138@qq.com>
2023-05-23 15:24:16 +0800

    {1090. 受标签影响的最大值}

---
+++ src/main/java/leetcode/One/Thousand/largestValsFromLabels/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.One.Thousand.largestValsFromLabels;
+
+/**
+ * @Desc:
+ * @Author: 泽露
+ * @Date: 2023/5/23 3:23 PM
+ * @Version: 1.initial version; 2023/5/23 3:23 PM
+ */
+public class Solution {
+    public int largestValsFromLabels(int[] values, int[] labels, int numWanted, int useLimit) {
+        int n = values.length;
+        Integer[] id = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+        Arrays.sort(id, (a, b) -> values[b] - values[a]);
+
+        int ans = 0, choose = 0;
+        Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n && choose < numWanted; ++i) {
+            int label = labels[id[i]];
+            if (cnt.getOrDefault(label, 0) == useLimit) {
+                continue;
+            }
+            ++choose;
+            ans += values[id[i]];
+            cnt.put(label, cnt.getOrDefault(label, 0) + 1);
+        }
+        return ans;
+    }
+}
