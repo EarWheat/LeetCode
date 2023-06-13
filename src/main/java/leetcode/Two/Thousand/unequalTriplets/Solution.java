9bca50d6f2b9fdd1bb081accc1c929152c3496eb
EarWheat <2636965138@qq.com>
2023-06-13 17:08:21 +0800

    {1043. 分隔数组以得到最大和}

---
+++ src/main/java/leetcode/Two/Thousand/unequalTriplets/Solution.java
@@ -0,0 +1,69 @@
+package leetcode.Two.Thousand.unequalTriplets;
+//给你一个下标从 0 开始的正整数数组 nums 。请你找出并统计满足下述条件的三元组 (i, j, k) 的数目：
+//
+//
+// 0 <= i < j < k < nums.length
+// nums[i]、nums[j] 和 nums[k] 两两不同 。
+//
+// 换句话说：nums[i] != nums[j]、nums[i] != nums[k] 且 nums[j] != nums[k] 。
+//
+//
+//
+//
+// 返回满足上述条件三元组的数目。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [4,4,2,4,3]
+//输出：3
+//解释：下面列出的三元组均满足题目条件：
+//- (0, 2, 4) 因为 4 != 2 != 3
+//- (1, 2, 4) 因为 4 != 2 != 3
+//- (2, 3, 4) 因为 2 != 4 != 3
+//共计 3 个三元组，返回 3 。
+//注意 (2, 0, 4) 不是有效的三元组，因为 2 > 0 。
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [1,1,1,1,1]
+//输出：0
+//解释：不存在满足条件的三元组，所以返回 0 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 3 <= nums.length <= 100
+// 1 <= nums[i] <= 1000
+//
+// Related Topics 数组 哈希表 👍 66 👎 0
+
+/**
+ * @Desc: 1043. 分隔数组以得到最大和
+ * @Author: 泽露
+ * @Date: 2023/6/13 5:07 PM
+ * @Version: 1.initial version; 2023/6/13 5:07 PM
+ */
+public class Solution {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length;
+        int[] d = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int maxValue = arr[i - 1];
+            for (int j = i - 1; j >= 0 && j >= i - k; j--) {
+                d[i] = Math.max(d[i], d[j] + maxValue * (i - j));
+                if (j > 0) {
+                    maxValue = Math.max(maxValue, arr[j - 1]);
+                }
+            }
+        }
+        return d[n];
+    }
+}
