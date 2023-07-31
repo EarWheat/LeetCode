4529cb8448a199d280c70b27ee708a2267807d81
EarWheat <2636965138@qq.com>
2023-07-31 16:35:11 +0800

    reorderList

--- src/main/java/leetcode/One/reorderList/reorderList.java
+++ src/main/java/leetcode/One/reorderList/reorderList.java
@@ -65,17 +65,4 @@ public class reorderList {
         temp.next = null;
     }
 
-    public static void main(String[] args) {
-        ListNode listNode1 = new ListNode(1);
-        ListNode listNode2 = new ListNode(2);
-        ListNode listNode3 = new ListNode(3);
-        ListNode listNode4 = new ListNode(4);
-        ListNode listNode5 = new ListNode(5);
-        listNode1.next = listNode2;
-        listNode2.next = listNode3;
-        listNode3.next = listNode4;
-        listNode4.next = listNode5;
-        reorderList(listNode1);
-        System.out.println(listNode1.next.val);
-    }
 }
