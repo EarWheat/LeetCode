6f703cd9d50865479130852e68a2b94e6e30e0f7
EarWheat <2636965138@qq.com>
2023-08-08 12:54:54 +0800

    {1749. 任意子数组和的绝对值的最大值}

---
+++ src/main/java/leetcode/One/Thousand/maxAbsoluteSum/Solution.java
@@ -0,0 +1,62 @@
+package leetcode.One.Thousand.maxAbsoluteSum;
+//给你一个整数数组 nums 。一个子数组 [numsl, numsl+1, ..., numsr-1, numsr] 的 和的绝对值 为 abs(
+//numsl + numsl+1 + ... + numsr-1 + numsr) 。
+//
+// 请你找出 nums 中 和的绝对值 最大的任意子数组（可能为空），并返回该 最大值 。
+//
+// abs(x) 定义如下：
+//
+//
+// 如果 x 是负整数，那么 abs(x) = -x 。
+// 如果 x 是非负整数，那么 abs(x) = x 。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,-3,2,3,-4]
+//输出：5
+//解释：子数组 [2,3] 和的绝对值最大，为 abs(2+3) = abs(5) = 5 。
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [2,-5,1,-4,3,-2]
+//输出：8
+//解释：子数组 [-5,1,-4] 和的绝对值最大，为 abs(-5+1-4) = abs(-8) = 8 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 10⁵
+// -10⁴ <= nums[i] <= 10⁴
+//
+// Related Topics 数组 动态规划 👍 76 👎 0
+
+/**
+ * @Desc: 1749. 任意子数组和的绝对值的最大值
+ * @Author: 泽露
+ * @Date: 2023/8/8 12:52 PM
+ * @Version: 1.initial version; 2023/8/8 12:52 PM
+ */
+public class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int positiveMax = 0, negativeMin = 0;
+        int positiveSum = 0, negativeSum = 0;
+        for (int num : nums) {
+            positiveSum += num;
+            positiveMax = Math.max(positiveMax, positiveSum);
+            positiveSum = Math.max(0, positiveSum);
+            negativeSum += num;
+            negativeMin = Math.min(negativeMin, negativeSum);
+            negativeSum = Math.min(0, negativeSum);
+        }
+        return Math.max(positiveMax, -negativeMin);
+    }
+}
