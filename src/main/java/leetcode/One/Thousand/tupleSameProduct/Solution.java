309149b3f5bfb5b6fb1928eae5907aec3c4f6256
EarWheat <2636965138@qq.com>
2023-10-19 14:49:32 +0800

    {1726. 同积元组}

---
+++ src/main/java/leetcode/One/Thousand/tupleSameProduct/Solution.java
@@ -0,0 +1,69 @@
+package leetcode.One.Thousand.tupleSameProduct;
+//给你一个由 不同 正整数组成的数组 nums ，请你返回满足 a * b = c * d 的元组 (a, b, c, d) 的数量。其中 a、b、c 和
+//d 都是 nums 中的元素，且 a != b != c != d 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [2,3,4,6]
+//输出：8
+//解释：存在 8 个满足题意的元组：
+//(2,6,3,4) , (2,6,4,3) , (6,2,3,4) , (6,2,4,3)
+//(3,4,2,6) , (4,3,2,6) , (3,4,6,2) , (4,3,6,2)
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [1,2,4,5,10]
+//输出：16
+//解释：存在 16 个满足题意的元组：
+//(1,10,2,5) , (1,10,5,2) , (10,1,2,5) , (10,1,5,2)
+//(2,5,1,10) , (2,5,10,1) , (5,2,1,10) , (5,2,10,1)
+//(2,10,4,5) , (2,10,5,4) , (10,2,4,5) , (10,2,5,4)
+//(4,5,2,10) , (4,5,10,2) , (5,4,2,10) , (5,4,10,2)
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 1000
+// 1 <= nums[i] <= 10⁴
+// nums 中的所有元素 互不相同
+//
+//
+// Related Topics 数组 哈希表 👍 77 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Desc:
+ * @Author: 泽露
+ * @Date: 2023/10/19 2:48 PM
+ * @Version: 1.initial version; 2023/10/19 2:48 PM
+ */
+public class Solution {
+    public int tupleSameProduct(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int key = nums[i] * nums[j];
+                cnt.put(key, cnt.getOrDefault(key, 0) + 1);
+            }
+        }
+        int ans = 0;
+        for (Integer v : cnt.values()) {
+            ans += v * (v - 1) * 4;
+        }
+        return ans;
+    }
+}
