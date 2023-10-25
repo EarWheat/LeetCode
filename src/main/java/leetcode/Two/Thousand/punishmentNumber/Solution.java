81cb05621666211bd3bc5a9966551e54d7b752ea
EarWheat <2636965138@qq.com>
2023-10-25 17:49:52 +0800

    求一个整数的惩罚数

---
+++ src/main/java/leetcode/Two/Thousand/punishmentNumber/Solution.java
@@ -0,0 +1,37 @@
+package leetcode.Two.Thousand.punishmentNumber;
+
+/**
+ * @Desc: 求一个整数的惩罚数
+ * @Author: 泽露
+ * @Date: 2023/10/25 5:49 PM
+ * @Version: 1.initial version; 2023/10/25 5:49 PM
+ */
+public class Solution {
+    public int punishmentNumber(int n) {
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            String s = Integer.toString(i * i);
+            if (dfs(s, 0, 0, i)) {
+                res += i * i;
+            }
+        }
+        return res;
+    }
+
+    public boolean dfs(String s, int pos, int tot, int target) {
+        if (pos == s.length()) {
+            return tot == target;
+        }
+        int sum = 0;
+        for (int i = pos; i < s.length(); i++) {
+            sum = sum * 10 + s.charAt(i) - '0';
+            if (sum + tot > target) {
+                break;
+            }
+            if (dfs(s, i + 1, sum + tot, target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
