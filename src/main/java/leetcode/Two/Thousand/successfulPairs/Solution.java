5b36d7fb9cd1895c9416d42a4afaadf10648a517
EarWheat <2636965138@qq.com>
2023-11-10 17:12:39 +0800

    咒语和药水的成功对数

---
+++ src/main/java/leetcode/Two/Thousand/successfulPairs/Solution.java
@@ -0,0 +1,37 @@
+package leetcode.Two.Thousand.successfulPairs;
+
+import java.util.Arrays;
+
+/**
+ * @Desc:
+ * @Author: 泽露
+ * @Date: 2023/11/10 5:10 PM
+ * @Version: 1.initial version; 2023/11/10 5:10 PM
+ */
+public class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int n = spells.length, m = potions.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            long t = (success + spells[i] - 1) / spells[i] - 1;
+            res[i] = m - binarySearch(potions, 0, m - 1, t);
+        }
+        return res;
+    }
+
+    public int binarySearch(int[] arr, int lo, int hi, long target) {
+        int res = hi + 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] > target) {
+                res = mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return res;
+    }
+}
+
