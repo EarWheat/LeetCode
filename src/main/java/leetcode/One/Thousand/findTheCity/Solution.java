366df783062030a55d8ab3c77dec4408756b1746
EarWheat <2636965138@qq.com>
2023-11-14 10:40:41 +0800

    {1334. 阈值距离内邻居最少的城市}

---
+++ src/main/java/leetcode/One/Thousand/findTheCity/Solution.java
@@ -0,0 +1,44 @@
+package leetcode.One.Thousand.findTheCity;
+
+import java.util.Arrays;
+
+/**
+ * @Desc: 1334. 阈值距离内邻居最少的城市
+ * @Author: 泽露
+ * @Date: 2023/11/14 10:40 AM
+ * @Version: 1.initial version; 2023/11/14 10:40 AM
+ */
+public class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[] ans = {Integer.MAX_VALUE / 2, -1};
+        int[][] mp = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(mp[i], Integer.MAX_VALUE / 2);
+        }
+        for (int[] eg : edges) {
+            int from = eg[0], to = eg[1], weight = eg[2];
+            mp[from][to] = mp[to][from] = weight;
+        }
+        for (int k = 0; k < n; ++k) {
+            mp[k][k] = 0;
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    mp[i][j] = Math.min(mp[i][j], mp[i][k] + mp[k][j]);
+                }
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int cnt = 0;
+            for (int j = 0; j < n; ++j) {
+                if (mp[i][j] <= distanceThreshold) {
+                    cnt++;
+                }
+            }
+            if (cnt <= ans[0]) {
+                ans[0] = cnt;
+                ans[1] = i;
+            }
+        }
+        return ans[1];
+    }
+}
