175965492996eb05e04892c3a8b56c9fd7cf7a51
EarWheat <2636965138@qq.com>
2024-02-04 16:57:30 +0800

    {292. Nim 游戏}

---
+++ src/main/java/leetcode/Two/canWinNim2/Solution.java
@@ -0,0 +1,13 @@
+package leetcode.Two.canWinNim2;
+
+/**
+ * @Desc: 292. Nim 游戏
+ * @Author: 泽露
+ * @Date: 2024/2/4 4:54 PM
+ * @Version: 1.initial version; 2024/2/4 4:54 PM
+ */
+public class Solution {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
