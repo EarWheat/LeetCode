5fc94b7799bb0018ce05d33e0db2107f80a7a5d3
EarWheat <2636965138@qq.com>
2024-02-29 18:18:23 +0800

    统计可能的树根数目

---
+++ src/main/java/leetcode/Two/Thousand/rootCount/Solution.java
@@ -0,0 +1,66 @@
+package leetcode.Two.Thousand.rootCount;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @Desc: 统计可能的树根数目
+ * @Author: 泽露
+ * @Date: 2024/2/29 6:07 PM
+ * @Version: 1.initial version; 2024/2/29 6:07 PM
+ */
+public class Solution {
+    int cnt = 0, res = 0;
+    int k;
+    List<Integer>[] g;
+    Set<Long> set;
+
+    public int rootCount(int[][] edges, int[][] guesses, int k) {
+        this.k = k;
+        int n = edges.length + 1;
+        g = new List[n];
+        for (int i = 0; i < n; i++) {
+            g[i] = new ArrayList<Integer>();
+        }
+        set = new HashSet<Long>();
+        for (int[] v : edges) {
+            g[v[0]].add(v[1]);
+            g[v[1]].add(v[0]);
+        }
+        for (int[] v : guesses) {
+            set.add(h(v[0], v[1]));
+        }
+
+        dfs(0, -1);
+        redfs(0, -1, cnt);
+        return res;
+    }
+
+    public long h(int x, int y) {
+        return (long) x << 20 | y;
+    }
+
+    public void dfs(int x, int fat) {
+        for (int y : g[x]) {
+            if (y == fat) {
+                continue;
+            }
+            cnt += set.contains(h(x, y)) ? 1 : 0;
+            dfs(y, x);
+        }
+    }
+
+    public void redfs(int x, int fat, int cnt) {
+        if (cnt >= k) {
+            res++;
+        }
+        for (int y : g[x]) {
+            if (y == fat) {
+                continue;
+            }
+            redfs(y, x, cnt - (set.contains(h(x, y)) ? 1 : 0) + (set.contains(h(y, x)) ? 1 : 0));
+        }
+    }
+}
